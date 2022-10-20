610497e4ddcd7803eb0888a5aa1535f1b42cae0e
y200090 <y200090@mail.ryukoku.ac.jp>
2022-10-20 13:14:50 +0900

    カラーコード追加

--- prob05.py
+++ prob05.py
@@ -1,15 +1,34 @@
 # ===============課題5 10/20===============
 
 with open('05.dat', 'r', encoding='utf-8') as f:
+    # ファイルの中身を一括で読み込み、改行文字で分割してリストに格納
     str = f.read().split()
 
-text = input('置換前部分文字列と置換後文字列を入力: ')
-temp = text.split()
-
-before = temp[0]
-after = temp[1]
-
+# 元の文字列
 for x in str:
+    print(x)
+print('\n', end='')
+
+entry = input('>>> 置換前部分文字列と置換後文字列を入力: ')
+print('\n', end='')
+order = entry.split()
+
+before = order[0]   # 置換前部分文字列
+after = order[1]    # 置換後文字列
+
+# 置換対象の文字列を表示
+for x in str:
+    # 文字列の存在を確認
     if before in x:
-        x = x.replace(before, after)
+        x = x.replace(before, '\033[34m' + before + '\033[0m')
+    print(x)
+
+print(' ↓')
+
+# 置換後の文字列(結果)を表示
+for x in str:
+    # 文字列の存在を確認
+    if before in x:
+        # 文字列を置換
+        x = x.replace(before, '\033[31m' + after + '\033[0m')
     print(x)
