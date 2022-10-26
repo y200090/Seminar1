79df9f132264be546d4d42a880f7edacdc997bbc
y200090 <y200090@mail.ryukoku.ac.jp>
2022-10-27 02:58:04 +0900

    2次元配列に読み込む工程を改良

--- adv04.py
+++ adv04.py
@@ -3,20 +3,25 @@
 a = []
 with open('04-adv.dat', 'r') as f:
     # 2次元配列に読み込む
-    while True:
-        s = f.readline()
-        if s == '':
-            break
+    for s in f:
         a.append(s.split())
+    
+    # while True:
+    #     s = f.readline()
+    #     if s == '':
+    #         break
+    #     a.append(s.split())
     
 print(f'リストの全容: {a}\n')
 
-for i in range(len(a)):
-    print(f'{i + 1}行目: {a[i]}')
+# for i in range(len(a)):
+#     print(f'{i + 1}行目: {a[i]}')
+for x in a:
+    print(x)
 
 print(f'\nリストの全要素: ', end='')
 for x in a:
     for y in x:
-        print(f'{y} ', end='')
+        print(y, end=' ')
 
 print('\n', end='')
