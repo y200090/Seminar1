73bfdb41513c5fd566af2c715a384784e1110670
y200090 <y200090@mail.ryukoku.ac.jp>
2022-10-27 15:27:42 +0900

    課題10

---
+++ prob10.py
@@ -0,0 +1,19 @@
+# ===============課題10 11/3===============
+
+words = ['机', '本', '机', '机', '本', '学校', '大学']
+
+# 重複した要素を持たないリストを作成
+unduplicated_words = list(set(words))
+
+appearance = []
+for str in unduplicated_words:
+    cnt = words.count(str)
+    appearance.append(cnt)
+
+# 2つのリストを辞書に変換
+dict_words = dict(zip(unduplicated_words, appearance))
+print(dict_words)
+print('\n', end='')
+
+for key in dict_words.keys():
+    print(f'{key}: {dict_words[key]}回')
