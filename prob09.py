7e601a56eb95f5de651362c65a3e26949a68f36b
y200090 <y200090@mail.ryukoku.ac.jp>
2022-10-27 15:27:34 +0900

    課題9

---
+++ prob09.py
@@ -0,0 +1,21 @@
+# ===============課題9 11/3===============
+
+words = ['机', '本', '机', '机', '本', '学校', '大学']
+
+# 重複した要素を削除したset型のオブジェクトを作成
+unduplicated_words = set(words)
+print(unduplicated_words)
+print('\n', end='')
+
+unduplicated_words = list(unduplicated_words)
+
+# dict_words = dict.fromkeys(words)
+# unduplicated_words = list(dict_words)
+
+# unduplicated_words = sorted(set(words), key=words.index)
+
+for i, str in enumerate(unduplicated_words, 1):
+    if i == len(unduplicated_words):
+        print(f'{str}')
+    else:
+        print(f'{str}', end=',')
