87d1ec289301a384d837413e9db1f549bd9d491c
y200090 <y200090@mail.ryukoku.ac.jp>
2022-10-31 17:40:22 +0900

    formatメソッドを使用した文字列差し込み機能を追加

--- prob08.py
+++ prob08.py
@@ -1,37 +1,41 @@
 # ===============課題8 10/27===============
 
-data = []
+sentence = '''
+龍谷大学先端理工学部{0}課程
+{1}さん
+こんにちは！'''
+
+datas = []
 with open('08a.dat', 'r', encoding='utf-8') as f:
     for line in f:
         dic = {}
-        n_key, n_value = line.split()
-        dic[n_key] = n_value
-        data.append(dic)
+        key, value = line.split()
+        print(sentence.format('\033[31m' + key + '\033[0m', '\033[31m' + value + '\033[0m'))
 
-# for str in data:
+# for data in datas:
 #     sentence = '''
 #     龍谷大学先端理工学部X課程
 #     Yさん
 #     こんにちは！'''
-#     for key in str.keys():
+#     for key in data.keys():
 #         sentence = sentence.replace('X', '\033[31m' + key + '\033[0m')
         
     
-#     for value in str.values():
+#     for value in data.values():
 #         sentence = sentence.replace('Y', '\033[31m' + value + '\033[0m')
     
 #     print(sentence, end='')
 # print('\n')
 
-for str in data:
-    sentence = '''
-    龍谷大学先端理工学部{X}課程
-    {Y}さん
-    こんにちは！'''
+# for data in datas:
+#     sentence = '''
+#     龍谷大学先端理工学部{X}課程
+#     {Y}さん
+#     こんにちは！'''
     
-    for key in str.keys():
-        sentence = sentence.format(X='\033[31m' + key + '\033[0m', Y='\033[31m' + str[key] + '\033[0m')
+#     for key in data.keys():
+#         sentence = sentence.format(X='\033[31m' + key + '\033[0m', Y='\033[31m' + data[key] + '\033[0m')
 
-    print(sentence, end='')
-print('\n')
+#     print(sentence, end='')
+# print('\n')
 
