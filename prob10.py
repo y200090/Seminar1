30039f58e47faaa41383391c34fa85aa5d095a4a
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-01 16:43:21 +0900

    Counterを追加

--- prob10.py
+++ prob10.py
@@ -1,5 +1,7 @@
 # ===============課題10 11/3===============
 
+from collections import Counter
+
 words = ['机', '本', '机', '机', '本', '学校', '大学']
 
 # 重複した要素を持たないリストを作成
@@ -7,13 +9,19 @@ unduplicated_words = list(set(words))
 
 appearance = []
 for word in unduplicated_words:
-    cnt = words.count(word)
-    appearance.append(cnt)
+    # 要素の出現回数を取得
+    apcount = words.count(word)
+    appearance.append(apcount)
 
 # 2つのリストを辞書に変換
 dict_words = dict(zip(unduplicated_words, appearance))
 print(dict_words)
 print('\n', end='')
 
-for key in dict_words.keys():
-    print(f'{key}: {dict_words[key]}回')
+# # キーに要素、値に出現回数を持つCounterオブジェクト(dictのサブクラス)を作成
+# dict_words = Counter(words)
+# # 出現回数順に並べ替えたリストを辞書に変換
+# print(dict(dict_words.most_common()), '\n')
+
+for key, value in dict_words.items():
+    print(f'{key}: {value}回')
