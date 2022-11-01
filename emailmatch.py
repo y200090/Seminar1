07ee71741b4bea1b4d9ebe3e564922da5857535e
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-01 16:44:35 +0900

    ダミーデータから正規表現でパターン化したE-メールを検索

---
+++ emailmatch.py
@@ -0,0 +1,42 @@
+# ===============正規表現 11/10===============
+
+# print(re.match('a\\b', 'a\\b'))
+# print(re.match(r'a\\b', 'a\\b'))  # パターンにrを付ける
+# print(re.match('a\\\\b', 'a\\b')) # パターンにエスケープ記号使用
+
+# print(re.match(r'\w+\\\w+', 'abcd\\efghij')) 
+
+import csv
+
+filename = 'dummy.csv'
+with open(filename, 'r', encoding='utf-8') as f:
+    dummy_data = csv.reader(f)
+    # 先頭行を飛ばす
+    next(dummy_data)
+
+    string_data = []
+    for row in dummy_data:
+        string_data.append(' '.join(row))
+
+    # string_data = ''
+    # for row in dummy_data:
+    #     string_data += ' '.join(row)
+    #     string_data += '\n'
+    # string_data = string_data.rstrip()
+
+import re
+
+# メールアドレスのパターン
+pattern = '[a-zA-Z0-9_.+-]+@[a-zA-z0-9-]+\.[a-zA-z0-9-.]+'
+repattern = re.compile(pattern)
+
+for sdata in string_data:
+    result = repattern.search(sdata)
+    if result:
+        print('Match String: ' + '\033[35m' + f'{result.group()}' + '\033[0m')
+    else:
+        print(f'Match String: None')
+
+# result = repattern.findall(string_data)
+# for target in result:
+#     print(target)
