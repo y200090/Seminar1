bf1905977029dac262b9399dac2689fe09898150
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-03 13:56:54 +0900

    joinを使って文字列化する手法を追加

--- prob09.py
+++ prob09.py
@@ -6,16 +6,20 @@ words = ['机', '本', '机', '机', '本', '学校', '大学']
 unduplicated_words = set(words)
 print(f'{unduplicated_words}\n')
 
-# リスト化
-unduplicated_words = list(unduplicated_words)
+# # リスト化
+# unduplicated_words = list(unduplicated_words)
 
 # dict_words = dict.fromkeys(words)
 # unduplicated_words = list(dict_words)
+# print(f'{unduplicated_words}\n')
 
 # unduplicated_words = sorted(set(words), key=words.index)
+# print(f'{unduplicated_words}\n')
 
-for i, word in enumerate(unduplicated_words, 1):
-    if i == len(unduplicated_words):
-        print(f'{word}')
-    else:
-        print(f'{word}', end=',')
+# for i, word in enumerate(unduplicated_words, 1):
+#     if i == len(unduplicated_words):
+#         print(f'{word}')
+#     else:
+#         print(f'{word}', end=',')
+
+print(','.join(unduplicated_words))
