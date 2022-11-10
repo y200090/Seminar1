8bcfc39af10fbfd023ce2260c101854689a981d2
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-10 11:23:29 +0900

    課題11

--- prob11.py
+++ prob11.py
@@ -11,14 +11,13 @@ def rd(fname, x):
 filename = ['11-1.dat', '11-2.dat']
 a = []
 b = []
+c = []
 
-for i, (fname, x) in enumerate(zip(filename, [a, b])):
+for fname, x in zip(filename, [a, b]):
     num = rd(fname, x)
     print(f'ファイル"{fname}"から・・・\n{num}: {x}\n')
 
-c = []
 c.extend([len(a), len(b), a, b])
-
 print(f'併合リスト:\n{c}\n')
 
 print('併合リストの各要素:\n' + ' '.join(list(map(str, c))))
