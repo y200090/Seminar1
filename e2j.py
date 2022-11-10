c2cafc42f9d6f5e7ae22c8734764c398dc4f1196
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-11 02:45:49 +0900

    辞書引き課題

--- e2j.py
+++ e2j.py
@@ -5,7 +5,6 @@ import csv, re
 
 with open('data/ej_sys.tsv', 'r', encoding='utf-8') as f:
     data = csv.reader(f, delimiter='\t')
-
     content = ''
     for line in data:
         content += '\t'.join(line) + '\n'
@@ -16,15 +15,15 @@ while True:
         print('>>> プログラムを終了します。')
         break
 
-    # パターンマッチした部分(r'\1')に'\'を追加してエスケープを行う
-    words = re.sub('([/\-.+*?^$({)}|])', r'\\\1', words)
-
     print('--------------------')
     
+    # パターンマッチした部分(r'\1')に'\'を追加してエスケープを行う
+    words = re.sub('([/\-.+*?^$({)}|])', r'\\\1', words)
+    results = re.finditer(f'^{words}\t.\t(.*)', content, re.MULTILINE)
     result = None
-    for result in re.finditer(f'^{words}\t.\t(.*)', content, re.MULTILINE):
+    for result in results:
         print(result.group(1))
-
+        
     # パターンマッチしなかった場合
     if result is None:
         print(result)
--- j2e.py
+++ j2e.py
@@ -5,7 +5,6 @@ import csv, re
 
 with open('data/je_sys.tsv', 'r', encoding='utf-8') as f:
     data = csv.reader(f, delimiter='\t')
-
     content = ''
     for line in data:
         content += '\t'.join(line) + '\n'
@@ -17,13 +16,14 @@ while True:
         break
         
     print('--------------------')
-    
-    result = None
-    for result in re.finditer(f'^{word}\t.\t(.*)', content, re.MULTILINE):
-        print(result.group(1))
 
+    results = re.finditer(f'^{word}\t.\t(.*)', content, re.MULTILINE)
+    result = None
+    for result in results:
+        print(result.group(1))
+        
     # パターンマッチしなかった場合
     if result is None:
         print(result)
-    
+
     print('--------------------\n')
