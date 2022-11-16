acd80901e60241ded7c0e3f2cea53f410f3b24fc
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-16 22:25:27 +0900

    コンパイル不要

--- prob14.py
+++ prob14.py
@@ -5,10 +5,11 @@ import re
 with open('data/14.dat', 'r', encoding='utf-8') as f:
     content = f.read()
 
-pattern = re.compile('([a-zA-Z]+):(\d+)')
-result = dict(pattern.findall(content))
+result = dict(re.findall('([a-zA-Z]+):(\d+)', content))
+
 for key, value in result.items():
     print(key, value)
 
+# 平均値
 avg = sum(list(map(int, result.values()))) / len(result)
 print(f'avg: {avg}')
