5014190d88321ef23371a5b58ef416eb9b8773ee
y200090 <y200090@mail.ryukoku.ac.jp>
2022-11-20 20:15:40 +0900

    tkinter next complete

--- adv2/tk.py
+++ adv2/tk.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 
 class Application(tk.Frame):
     def __init__(self, master):
@@ -11,31 +10,37 @@ class Application(tk.Frame):
         self.widgets()
 
     def widgets(self):
-        self.result_word = tk.StringVar(self.master)
-
         # フレーム1
-        self.frame1 = ttk.Frame(self.master)
-        self.frame1.pack(side=tk.TOP, padx=30, pady=20, fill=tk.BOTH)
-        
-        self.search_icon = tk.PhotoImage(file='../assets/search_icon.png').subsample(2, 2)
-        search_label = ttk.Label(self.frame1, image=self.search_icon)
-        search_label.pack(side=tk.LEFT)
+        self.frame1 = tk.Frame(self.master, width=900, height=30)
+        self.frame1.pack(side=tk.TOP, padx=51, pady=40, fill=tk.BOTH)
 
-        self.font =('', 18)
-        self.search_text = ttk.Entry(self.frame1, font=self.font, width=30, textvariable=self.result_word)
-        self.search_text.pack(side=tk.LEFT)
+        self.font1 =('', 18)
+        self.search_text = tk.Entry(self.frame1, font=self.font1, width=30, relief=tk.SOLID, bd=1)
+        self.search_text.pack(side=tk.LEFT, ipady=3)
 
+        # 削除ボタン
         self.clear_icon = tk.PhotoImage(file='../assets/close_icon.png').subsample(2, 2)
-        self.clear_button = ttk.Button(self.frame1, image=self.clear_icon)
-        self.clear_button['command'] = lambda: self.search_text.delete(0, tk.END)
+        self.clear_button = tk.Button(self.frame1, image=self.clear_icon, relief=tk.FLAT, width=30, height=30)
+        self.clear_button['command'] = lambda: [
+            self.search_text.delete(0, tk.END),
+            self.textbox.delete(1.0, tk.END)
+        ]
         self.clear_button.pack(side=tk.LEFT)
-        
-        # フレーム2
-        self.frame2 = tk.Frame(self.master, width=900, height=400, bg="red")
-        self.frame2.pack(side=tk.TOP, padx=30, fill=tk.BOTH)
 
-        self.result = ttk.Label(self.frame2, textvariable=self.result_word)
-        self.result.pack(side=tk.TOP)
+        # 検索ボタン
+        self.search_icon = tk.PhotoImage(file='../assets/search_icon.png').subsample(2, 2)
+        self.search_button = tk.Button(self.frame1, image=self.search_icon, relief=tk.FLAT, width=30, height=30)
+        self.search_button['command'] = lambda: self.textbox.insert('1.0', self.search_text.get())
+        self.search_button.pack(side=tk.LEFT)
+
+        # フレーム2
+        self.frame2 = tk.Frame(self.master, width=900)
+        self.frame2.pack(side=tk.TOP, padx=30)
+
+        self.font2 = ('', 16)
+        self.textbox = tk.Text(self.frame2, font=self.font2, width=72, height=17, relief=tk.SOLID, bd=1)
+        self.textbox.pack(side=tk.TOP)
+
 
 if __name__ == '__main__':
     root = tk.Tk()
